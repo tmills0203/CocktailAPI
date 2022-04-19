2875542c4a326dabc4e9f7b685f492cfe0f57df2
Theresa Mills <tmills172@hotmail.com>
2022-04-19 17:37:23 -0500

    add drink name placeholder

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`)
   .then((data) => {
     // console.log(data.drinks);
     const imgContainer = document.querySelector(".img-container");
+    const carousel = document.querySelector(".carousel");
 
     data.drinks.forEach((obj) => {
       //   console.log(obj);
       let [drink, drinkImg] = Object.values(obj);
       console.log(drinkImg);
 
-      let html = ` <img src="${drinkImg}" alt="" />`;
-      imgContainer.insertAdjacentHTML("beforeend", html);
+      let html = `  
+      <section id="imgs" class="img-container">
+        <img src="${drinkImg}" alt="" />
+      </section>
+      <h2 id="drinkName">${drink}</h2>`;
+      carousel.insertAdjacentHTML("beforeend", html);
     });
   })
   .catch((err) => {
