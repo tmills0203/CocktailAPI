713fdc5d559fc647129c9dce3e5a1ad13e65ed65
Theresa Mills <tmills172@hotmail.com>
2022-04-19 21:16:34 -0500

    add style

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 "use strict";
+const imgContainer = document.querySelector(".img-container");
+const h2 = document.querySelector("h2");
+let img = document.querySelectorAll("#imgs img");
+console.log(img);
 
 fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`)
   .then((res) => res.json()) // parse response as JSON
   .then((data) => {
-    // console.log(data.drinks);
-    const imgContainer = document.querySelector(".img-container");
-    const carousel = document.querySelector(".carousel");
-
     data.drinks.forEach((obj) => {
-      //   console.log(obj);
       let [drink, drinkImg] = Object.values(obj);
-      console.log(drinkImg);
+      let html = `<img src="${drinkImg}" alt="" />`;
 
-      let html = `  
-      <section id="imgs" class="img-container">
-        <img src="${drinkImg}" alt="" />
-      </section>
-      <h2 id="drinkName">${drink}</h2>`;
-      carousel.insertAdjacentHTML("beforeend", html);
+      //////
+      imgContainer.insertAdjacentHTML("beforeend", html);
+      /////
+      let index = 0;
+      setInterval(2000);
+      /////
+      index++;
+      /////
+      if (index > img.length) {
+        index = 0;
+      } else if (index < 0) {
+        index = img.length;
+      }
+
+      imgContainer.style.transform = `translateX(${-index * 500}px)`;
     });
   })
   .catch((err) => {
